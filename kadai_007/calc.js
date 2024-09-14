18b0533f0a71e0de6a1804a4df002bc73119345a
Abe5960 <codex2700@gmail.com>
2024-09-14 22:22:45 +0900

    ７章課題です

---
+++ kadai_007/calc.js
@@ -0,0 +1,22 @@
+let num = Math.floor(Math.random()*16) ;
+
+if (num ===3 || num ===6 || num ===9 || num ===12 || num === 15) {
+    console.log('3の倍数です');
+}
+
+else if (num === 5 || num === 10 || num ===15 ) {
+    console.log('5の倍数です');
+}
+
+else if (num === 15)  {
+    console.log('3と5の倍数です');
+}
+
+else if (num ===0 ) {
+    console.log=(1);
+}
+
+else 
+{
+    console.log(num);   
+}

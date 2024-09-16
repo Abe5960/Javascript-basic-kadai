294d592a80b870f3ad39cc3268f37c7446608aab
Abe5960 <codex2700@gmail.com>
2024-09-16 20:54:24 +0900

    7章　再々提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,16 +4,17 @@ function getRandomInt(min, max) {
   
 let num =getRandomInt(1, 100);
 
-if (num % 3 === 0 ) {
-    console.log('3の倍数です');
+if (num % 3 ===0 && num % 5 ===0 )  {
+    console.log('3と5の倍数です');
 }
+
 
 else if (num % 5 === 0 ) {
     console.log('5の倍数です');
 }
 
-else if (num % 3 ===0 && num % 5 ===0 )  {
-    console.log('3と5の倍数です');
+else if (num % 3 === 0 ) {
+    console.log('3の倍数です');
 }
 
 else {
